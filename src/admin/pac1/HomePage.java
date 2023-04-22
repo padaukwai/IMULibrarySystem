3da2235f806fc087629a96d9d968da3db6bfdb58
IMU <IMU@DESKTOP-FSRQE0S>
2023-04-22 13:28:54 +0630

    Version 1

---
+++ src/admin/pac1/HomePage.java
@@ -0,0 +1,10 @@
+package admin.pac1;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome from IMU Admin Department");
+	}
+
+}
